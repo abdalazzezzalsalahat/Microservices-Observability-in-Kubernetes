6f607cad7f6d405603745319083b6eb3fb6a99a5
abdalaziz <abdalazzezzalsalahat@icloud.com>
2025-02-20 01:37:47 +0300

    added jaeger and opentelementry for tracing and collecting app data

--- length_service/main.py
+++ length_service/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import logging
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
 app = FastAPI()
 
 logging.basicConfig(level=logging.INFO)
+
+trace.set_tracer_provider(TracerProvider())
+tracer = trace.get_tracer(__name__)
+
+jaeger_exporter = JaegerExporter(agent_host_name="jaeger", agent_port=6831)
+span_processor = BatchSpanProcessor(jaeger_exporter)
+trace.get_tracer_provider().add_span_processor(span_processor)
 
 
 class LengthRequest(BaseModel):
